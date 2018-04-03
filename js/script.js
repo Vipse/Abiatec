3f182cf2dc5883e1ea41dfc30c74ca98ca1444f7
Vladimir Zubritskiy <Vovavipse@gmail.com>
2018-04-03 13:27:21 +0300

    completed main page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
-$(".burger").on("click", function () {
-    $(".headerMenu ul").slideToggle("200");
+// Phones toggling
+$(".showPhones").on("click", function () {
+    $(".phonesMobileVersion").slideToggle("150");
+    $(this).toggleClass("cross");
 });
+
+// Mobile menu toggling
+
+$(".burger").click(function() {
+    $(".mobileMenuWrapper").fadeIn("fast");
+    $( ".slideMenuMobile" ).toggle( "slide" );
+});
+$(".mobileMenuWrapper").click(function(event) {
+    $( ".slideMenuMobile" ).hide( "slide" );
+    $(this).fadeOut("fast");
+    event.stopPropagation();
+});
+$(".closeMenu").click(function() {
+    $( ".slideMenuMobile" ).hide( "slide" );
+    $(".mobileMenuWrapper").fadeOut("fast");
+    event.stopPropagation();
+});
+$(".slideMenuMobile").click(function() {
+    event.stopPropagation();
+});
+
+
+//Forms toggling
+$(".header .askQuestionButton").on("click", function () {
+    $(".header .formConsult").slideToggle("150");
+    $(this).toggleClass("cross");
+});
+
+$(".header .closeForm").on("click", function () {
+  $(".header .formConsult").slideUp("150");
+  $(".header .askQuestionButton").removeClass("cross");
+});
+
+
+$(".bottomQuestion .askQuestionButton").on("click", function () {
+  var form = $("footer .formConsult");
+  form.slideToggle("150");
+  if (!form.hasClass("activeForm")) {
+    var destination = ($("#bottomForm").offset().top) - 120;
+    $('html').animate({scrollTop: destination}, 1100);
+  }
+  form.toggleClass("activeForm");
+});
+
+$("footer .closeForm").on("click", function () {
+  $("footer .formConsult").slideUp("150").removeClass("activeForm");
+
+
+});
+
